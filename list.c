0ef6e83d80b07a9d7fbae63a2dc19cae08e6d621
TahsinAhmed13 <tahsinahmed217@gmail.com>
2020-11-03 16:42:15 -0500

    add inserting operations

--- list.c
+++ list.c
@@ -35,3 +35,34 @@ struct song_node *new_song(char *name, char *artist)
     strcpy(song->artist, artist); 
     return song; 
 }
+
+struct song_node *add_top(char *name, char *artist, struct song_node *songs)
+{
+    struct song_node *my_song = new_song(name, artist); 
+    my_song->next = songs; 
+    return my_song; 
+}
+
+struct song_node *add_order(char *name, char *artist, struct song_node *songs)
+{
+    struct song_node *dummy = new_song("", ""); 
+    dummy->next = songs; 
+    struct song_node *my_song = new_song(name, artist); 
+    struct song_node *last = dummy; 
+    while(songs)
+    {
+        int cmp = strcmp(my_song->artist, songs->artist); 
+        int cmp2 = strcmp(my_song->name, songs->name); 
+        if(cmp < 0 || (cmp == 0 && cmp2 <= 0))
+        {
+            last->next = my_song; 
+            my_song->next = songs; 
+            break; 
+        }
+        last = songs; 
+        songs = songs->next; 
+    }
+    struct song_node *first = dummy->next; 
+    free(dummy); 
+    return first; 
+}
--- list.h
+++ list.h
@@ -5,9 +5,9 @@ struct song_node
     struct song_node *next; 
 };
 
-void print_song(struct song_node *);
 void print_songs(struct song_node *);
 
 struct song_node *free_songs(struct song_node *); 
 
-struct song_node *add_top(struct song_node *); 
+struct song_node *add_top(char *, char *, struct song_node *); 
+struct song_node *add_order(char *, char *, struct song_node *); 
