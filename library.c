bc4900e9c0c8c7aa337d7278d99f368762dca609
TahsinAhmed13 <tahsinahmed217@gmail.com>
2020-11-11 00:04:28 -0500

    stylistic changes

--- library.c
+++ library.c
@@ -10,13 +10,11 @@ struct song_node **create_library()
     return lib; 
 }
 
-int get_index(char *artist)
+int get_index(char c)
 {
-    char initial = artist[0]; 
-    if('a' <= initial && initial <= 'z')
-        return initial - 'a' + 1; 
+    if('a' <= c && c <= 'z')
+        return c - 'a' + 1; 
     return 0; 
-    return initial - 'a'; 
 }
 
 char get_initial(int i)
@@ -29,19 +27,17 @@ char get_initial(int i)
 void print_letter_library(struct song_node **lib, char c) 
 {
     printf("printing songs in '%c' list\n\t", c);
-    print_songs(lib[get_index(&c)]);
+    print_songs(lib[get_index(c)]);
 }
 
 void print_artist_library(struct song_node **lib, char *artist)
 {
-    struct song_node *s = lib[get_index(artist)]; 
+    struct song_node *s = lib[get_index(artist[0])]; 
     printf("printing songs with artist '%s'\n\t", artist);
     while(s)
     {
-        if(!strcmp(s->artist, artist)){
+        if(!strcmp(s->artist, artist))
             print_song(s);  
-        }
-        
         s = s->next; 
     }
 }
@@ -58,7 +54,7 @@ void print_library(struct song_node **lib)
 
 struct song_node **add_to_library(struct song_node **lib, char *artist, char *name)
 {
-    int i = get_index(artist); 
+    int i = get_index(artist[0]); 
     printf("adding [%s, %s] to '%c' list\n\t", artist, name, get_initial(i));  
     lib[i] = add_order(lib[i], artist, name); 
     return lib; 
@@ -66,20 +62,21 @@ struct song_node **add_to_library(struct song_node **lib, char *artist, char *na
 
 struct song_node *find_song_library(struct song_node **lib, char *artist, char *name)
 {
-    int i = get_index(artist); 
+    int i = get_index(artist[0]); 
     printf("finding [%s, %s] in '%c' list\n\t", artist, name, get_initial(i));  
     return find_song(lib[i], artist, name); 
 }
 
 struct song_node *find_artist_library(struct song_node **lib, char *artist)
 {
-    int i = get_index(artist); 
+    int i = get_index(artist[0]); 
     printf("finding artist %s in '%c' list\n\t", artist, get_initial(i));   
     return find_artist(lib[i], artist); 
 }
 
-struct song_node *del_song_library(struct song_node **lib, char *artist, char *name){
-    remove_song(lib[get_index(artist)], artist, name);
+struct song_node *del_song_library(struct song_node **lib, char *artist, char *name)
+{
+    remove_song(lib[get_index(artist[0])], artist, name);
 }
 
 struct song_node **free_library(struct song_node **lib)
